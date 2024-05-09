0f10b9027de5e8e708ace2a85155f3b59b1a7101
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-05-09 15:47:44 +0100

    feat: Add types for OptionsObject, Separator, and RatioForm

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type OptionsObject = {
+  title: string;
+  subtitle: string;
+};
+
+export type Separator = "Colon" | "Slash";
+
+export type RatioForm = "Ratio Form" | "Reduced Ratio Form" | "Decimal Form";
