97ab5c3924df107b1d35f50540a73dea6e9f2846
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-08-26 10:52:07 +0100

    chore: Update tailwind.config.js with new color "blue-violet"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-violet": {
+          50: "#efeeff",
+          100: "#e2e0ff",
+          200: "#cac7fe",
+          300: "#aaa5fc",
+          400: "#8881f8",
+          500: "#6b63f1",
+          600: "#4f46e5",
+          700: "#4038ca",
+          800: "#3730a3",
+          900: "#332e81",
+          950: "#1e1b4b",
+        },
+      },
+    },
   },
   plugins: [],
-  darkMode: ['class', '.figma-dark']
-}
+  darkMode: ["class", ".figma-dark"],
+};
