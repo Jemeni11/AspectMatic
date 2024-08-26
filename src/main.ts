a1fbdae4c0dd2baf557da317f798373ebaa0982b
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-08-26 16:21:59 +0100

    feat: Add event listeners for aspect ratio calculation and closing UI

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { showUI } from "@create-figma-plugin/utilities";
+import { showUI, on } from "@create-figma-plugin/utilities";
+import handleAspectRatioCalculation from "./helpers/getAspectRatio";
 
 export default function () {
+  on("ASPECT_RATIO_CALCULATION", handleAspectRatioCalculation);
+
+  on("CLOSE_UI", function () {
+    figma.closePlugin();
+  });
+
   showUI({
     height: 550,
     width: 350,
