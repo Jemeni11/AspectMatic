af5d5bbc51a2d2ad501dad2487540e1b0c503bc0
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-08-31 09:56:39 +0100

    feat: Add helper functions for aspect ratio manipulation

---
+++ src/helpers/index.ts
@@ -0,0 +1,13 @@
+import copyToClipboard from "./copyToClipboard";
+import decimalFormConverter from "./decimalFormConverter";
+import formatAspectRatio from "./formatAspectRatio";
+import getAspectRatio from "./getAspectRatio";
+import getReducedRatio from "./getReducedRatio";
+
+export {
+  copyToClipboard,
+  decimalFormConverter,
+  formatAspectRatio,
+  getAspectRatio,
+  getReducedRatio,
+};
