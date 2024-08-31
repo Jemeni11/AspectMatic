f595a4fbbc6adb2c792ad9d65919ad4831e71336
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-08-31 09:51:47 +0100

    feat: Update types.ts with new type definitions for OptionsObject, RatioForm, and AspectRatio.

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,26 @@
-export type OptionsObject = {
-  title: string;
+export type SeparatorOption = {
+  title: Separator;
+  subtitle: string;
+};
+
+export type RatioFormOption = {
+  title: RatioForm;
+  subtitle: string;
+};
+
+export type OptionsObject<T extends Separator | RatioForm> = {
+  title: T;
   subtitle: string;
 };
 
 export type Separator = "Colon" | "Slash";
 
-export type RatioForm = "Ratio" | "Reduced Ratio" | "Decimal";
+export type RatioForm = "Regular Ratio" | "Reduced Ratio" | "Decimal";
+
+export type AspectRatio = {
+  nodeName: string;
+  aspectRatio: {
+    width: number;
+    height: number;
+  };
+};
