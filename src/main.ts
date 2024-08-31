32fcde28167f7bf9ed348746e948d309f2fc4ca5
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-08-31 10:08:26 +0100

    refactor: Update main.ts to use getAspectRatio helper function for aspect ratio calculation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { showUI, on } from "@create-figma-plugin/utilities";
-import handleAspectRatioCalculation from "./helpers/getAspectRatio";
+import { getAspectRatio } from "./helpers";
 
 export default function () {
-  on("ASPECT_RATIO_CALCULATION", handleAspectRatioCalculation);
+  on("ASPECT_RATIO_CALCULATION", getAspectRatio);
 
   on("CLOSE_UI", function () {
     figma.closePlugin();
