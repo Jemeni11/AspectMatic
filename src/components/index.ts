8e15792f2ed2dd00ce4d20af90c575aad5f5831b
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-09-05 14:10:48 +0100

    feat: Add HistoryModal, DropdownMenu, DropdownSection, DropdownItem, and Table components to components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,13 @@
 import RadioGroup from "./RadioGroup";
+import HistoryModal from "./HistoryModal";
+import { DropdownMenu, DropdownSection, DropdownItem } from "./Dropdown";
+import Table from "./Table";
 
-export { RadioGroup };
+export {
+  RadioGroup,
+  HistoryModal,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+  Table,
+};
