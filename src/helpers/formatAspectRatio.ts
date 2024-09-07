9ac595c7a5d3b80c7942178bf8d08c3d6b9bea29
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-09-07 14:29:42 +0100

    Refactor: deleted formatAspectRatio, renamed allAspectRatioFormats to formatAspectRatio.

--- src/helpers/formatAspectRatio.ts
+++ src/helpers/formatAspectRatio.ts
@@ -1,42 +1,7 @@
 import { getReducedRatio, decimalFormConverter } from "./";
-import type { RatioForm, Separator, AllAspectRatioFormats } from "../types";
+import type { AllAspectRatioFormats } from "../types";
 
-/**
- * Formats an aspect ratio based on user-selected options.
- * @param width - The width of the aspect ratio
- * @param height - The height of the aspect ratio
- * @param ratioForm - "Regular Ratio" | "Reduced Ratio" | "Decimal"
- * @param separatorForm - "Colon" | "Slash"
- * @returns A string representation of the formatted aspect ratio
- */
 export default function formatAspectRatio(
-  width: number,
-  height: number,
-  ratioForm: RatioForm,
-  separatorForm: Separator,
-): string {
-  const separator = separatorForm === "Colon" ? ":" : "/";
-
-  switch (ratioForm) {
-    case "Regular Ratio":
-      return `${width}${separator}${height}`;
-
-    case "Reduced Ratio":
-      const [reducedWidth, reducedHeight] = getReducedRatio(width, height);
-      return `${reducedWidth}${separator}${reducedHeight}`;
-
-    case "Decimal":
-      return decimalFormConverter(width, height).toString();
-
-    case "Rounded Decimal":
-      return decimalFormConverter(width, height, true).toString();
-
-    default:
-      throw new Error("Invalid ratio form selected");
-  }
-}
-
-export function allAspectRatioFormats(
   width: number,
   height: number,
 ): AllAspectRatioFormats {
